c786fab792d5be1fe14823e35d124804eb434c8f
David Wong <davwong@emi.u-bordeaux1.fr>
2014-01-24 15:35:40 +0100

    genkey not tested

--- src/genkey.c
+++ src/genkey.c
@@ -1,2 +1,24 @@
-AES-whitebox
-============
+#include <time.h>
+#include <stdlib.h>
+
+#include <genkey.h>
+
+static int genkey()
+{
+    srand(time(NULL));
+    int rr;
+    bool key[64];
+
+    for(int ii = 0; ii < 64; ii++)
+    {
+	rr = rand();
+	key[ii] = rr;
+    }
+
+}
+
+int main()
+{
+    bool truc[64] = genkey();
+    printf("%s", truc);
+}
