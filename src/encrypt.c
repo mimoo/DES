f1b7d77faf7b157a0183b1cc1078517e72a2fd73
Jacques Monin <jacmonin@emi.u-bordeaux1.fr>
2014-01-29 16:05:01 +0100

    reading input file

--- src/encrypt.c
+++ src/encrypt.c
@@ -1,33 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include "encrypt.h"
 
-#define BOOL_FMT(bool_expr) "%s",  (bool_expr) ? "1" : "0"
 
-struct sBlock
-{
-	bool bit[64];
-};
-typedef struct sBlock Block;
 
-struct sMessage
-{
-	Block * block;
-	int taille;
-};
-typedef struct sMessage Message;
-
-void encrypt()
+void encrypt(FILE * inputFile)
 {
 	int i;
-	Message * m = malloc(sizeof(Message));
-	FILE * fichier = fopen("a.txt", "rb");
-	if(fichier == NULL)
-		exit(EXIT_FAILURE);
-	m[0].block=malloc(sizeof(Block));
-        for(i=0;i<64;i++)
-	{
-		fread(&m[0].block->bit[i],1, 1, fichier);
-		printf(BOOL_FMT(m[0].block->bit[i]));
-	}
+	char block[8];
+	while (fgets(block, 8, inputFile) != NULL) 
+        {
+		for(i=0;i<8;i++)
+		{
+			fprintf(stdout,"%c",block[i]);
+		}
+        }
 }
