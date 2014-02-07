fb7b1730ee056b48af126cf8572d34f3c209dd85
David Wong <davwong@emi.u-bordeaux1.fr>
2014-02-07 16:30:28 +0100

    added printbits function, can be usefull to test

--- src/DES.c
+++ src/DES.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -5,7 +6,7 @@
 #include "DES.h"
 
 // Verify if the parity bits are okay
-void key_parity_verify(uint64_t* key)
+bool key_parity_verify(uint64_t key)
 {
     int jj;
     int parity_bit = 0;
@@ -20,25 +21,26 @@ void key_parity_verify(uint64_t* key)
 	    if(parity_bit == 0)
 	    {
 		// test if ii-th bit != 0
-		if( (key << ii) & 0x80000000 != (uint64_t)0)
+		if( ((key << ii) & firstbit) != (uint64_t)0)
 		    return false;
 	    }
 	    else
 	    {
 		// test if ii-th bit != 1
-		if( (key << ii) & 0x80000000 != 0x80000000)
+		if( ((key << ii) & firstbit) != firstbit)
 		    return false;
 	    }
 	    parity_bit = 0; // re-init parity_bit for next byte block
 	}
 	else
 	{
-	    if( (key << ii) & 0x80000000 == 0x80000000)
+	    if( ((key << ii) & firstbit) == firstbit)
 	    {
 		parity_bit = parity_bit == 0 ? 1 : 0;
 	    }
 	}
     }
+
     return true;
 }
 
@@ -49,7 +51,7 @@ void key_schedule(uint64_t* key)
     uint64_t key_temp;
 
     // test key parity bits
-    if(!key_parity_verify)
+    if(!key_parity_verify(*key))
     {
 	printf("The key you used is malformated : parity bits incorrect\n");
 	exit(EXIT_FAILURE);
@@ -67,14 +69,34 @@ void key_schedule(uint64_t* key)
 }
 
 // function to print a char in binary
-void printbits(unsigned char v) {
-   for(int ii = 7; ii >= 0; ii--) putchar('0' + ((v >> ii) & 1));
+void printbits(uint64_t v) {
+    for(int ii = 0; ii < 64; ii++)
+    {
+	if( ((v << ii) & firstbit) == (uint64_t)0)
+	    printf("0");
+	else
+	    printf("1");
+    }
 }
 
 // test only
 int main()
 {
-   
+    uint64_t key_test = 0b0110000010110110011001101111101000111111111010001011000110001110;
+
+    printbits(key_test);
+
+    printf("\n");
+
+    if(key_parity_verify(key_test) == true)
+    {
+	printf("yes");
+    }
+    else
+    {
+	printf("no");
+    }
+    printf("\n");
     
     return EXIT_SUCCESS;
 }
